3205ed6a352df04bf05b6136d3ffffb8998079dc
Luis Roldan <hichi8520@gmail.com>
2021-10-05 17:10:44 -0600

    ZigZag tested in console

--- Library-LAB4/ZigZag.cs
+++ Library-LAB4/ZigZag.cs
@@ -12,27 +12,27 @@ namespace Library_LAB4
 
         public bool Cifrar(string rutaArchivo, string rutaLlave, string rutaCifrado, string nombreArchivo)
         {
-            
-            //try
-            //{
+
+            try
+            {
                 // LEER ARCHIVO CON LLAVE
                 llave = Convert.ToInt32(File.ReadAllText(rutaLlave));
                 Console.WriteLine("Llave: " + llave);
 
                 escribirCifrado(rutaArchivo, rutaCifrado, nombreArchivo);
                 return true;
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
-            //    return false;
-            //}
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
         }
 
         private void escribirCifrado(string rutaArchivo, string rutaCifrado, string nombreArchivo)
         {
-            //try
-            //{
+            try
+            {
                 using var Fs = new FileStream(rutaArchivo, FileMode.Open);
                 using BinaryReader Br = new BinaryReader(Fs);
                 using FileStream writeStream = new FileStream($"{rutaCifrado}/{nombreArchivo}.zz", FileMode.OpenOrCreate);
@@ -44,11 +44,6 @@ namespace Library_LAB4
 
                 while (Br.BaseStream.Position != Br.BaseStream.Length)
                 {
-                    byteBuffer = Br.ReadBytes(buffer);
-                    //foreach (var item in byteBuffer)
-                    //{
-
-                    //}
 
                     int actual = 0;
                     while (Br.BaseStream.Position != Br.BaseStream.Length && actual < llave - 1)
@@ -64,14 +59,14 @@ namespace Library_LAB4
                     }
                 }
 
-                llave = 0;
-
                 for (int i = 0; i < llave; i++) if (ZigZag[i] != null) Bw.Write(Encoding.UTF8.GetBytes(ZigZag[i]));
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex);
-            //}
+
+                llave = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         public bool Descifrar(string rutaCifrado, string rutaLlave, string rutaDescifrado, string nombreArchivo)
@@ -92,37 +87,37 @@ namespace Library_LAB4
             }
         }
 
-        private string[] DText(BinaryReader br, int key, int TotalChars)
+        private string[] DescifrarTexto(BinaryReader br, int llave, int CharsTotales)
         {
-            string[] ZigZag = new string[key];
+            string[] ZigZag = new string[llave];
             ZigZag.Initialize();
 
-            int n = (int)Math.Ceiling((double)((TotalChars - 1.0) / (2 * (key - 1))) + 1);
-            int MissedChars = (int)(n + (key - 2) * (2 * (n - 1)) + (n - 1) - TotalChars);
+            int n = (int)Math.Ceiling((double)((CharsTotales - 1.0) / (2 * (llave - 1))) + 1);
+            int CharsOmitidos = (int)(n + (llave - 2) * (2 * (n - 1)) + (n - 1) - CharsTotales);
 
 
-            //For Top Line || If Missing Spaces != 0 |=>| Fill Top - 1, else fill Top
-            if (MissedChars != 0) ZigZag[0] = GetString(br.ReadChars(n - 1));
+            //Fila superior || If Chars Omitidos != 0 |=>| Llenar Top - 1, else Llenar Top
+            if (CharsOmitidos != 0) ZigZag[0] = GetString(br.ReadChars(n - 1));
             else ZigZag[0] = GetString(br.ReadChars(n));
 
 
-            //If the key its bigger than two, and missing characters >= 2 |=>| Fill second - 1
-            if (key > 2)
+            //Si la llave es mayor a 2, y chars omitidos >= 2 |=>| Llenar segunda fila - 1
+            if (llave > 2)
             {
-                if (MissedChars >= 2) ZigZag[1] = GetString(br.ReadChars(2 * n - 3));
+                if (CharsOmitidos >= 2) ZigZag[1] = GetString(br.ReadChars(2 * n - 3));
                 else ZigZag[1] = GetString(br.ReadChars(2 * n - 2));
             }
 
-            //If the key its bigger than three, check if "i" line needs to be Fiiled (complete || complete - 1 || complete - 2) 
-            if (key > 3)
+            //Si la llave es mayor a 3, ver si la línea "i" necesita ser llenada (completo || completo - 1 || completo - 2) 
+            if (llave > 3)
             {
-                for (int i = 2; i < key - 1; i++)
+                for (int i = 2; i < llave - 1; i++)
                 {
-                    //This if valuates if i need to skip characters or not
-                    if ((key - MissedChars < 0) || MissedChars >= (i + 1))//If its negative that means that in all lines have to skip one or two || If missed chars in last column are more that current line
+                    //Evaluar si saltar caracteres o no
+                    if ((llave - CharsOmitidos < 0) || CharsOmitidos >= (i + 1))//Si es negativo en todas las lineas hay que saltar uno o dos
                     {
-                        //This valuates if i need to skip 1 or two /*If true |=>| two, else |=>| one*/
-                        if (MissedChars >= (key + (key - (i + 1)))) ZigZag[i] = GetString(br.ReadChars(2 * n - 4));
+                        //Evaluar si se debe saltar una o dos /*If true |=>| dos, else |=>| una*/
+                        if (CharsOmitidos >= (llave + (llave - (i + 1)))) ZigZag[i] = GetString(br.ReadChars(2 * n - 4));
                         else ZigZag[i] = GetString(br.ReadChars(2 * n - 3));
                     }
                     else ZigZag[i] = GetString(br.ReadChars(2 * n - 2));
@@ -130,9 +125,9 @@ namespace Library_LAB4
             }
 
 
-            //For Bottom Line, If MissingSpaces < Key |=>| Fill Last, Else FIll Last - 1
-            if (MissedChars > key) ZigZag[key - 1] = GetString(br.ReadChars(n - 2));
-            else ZigZag[key - 1] = GetString(br.ReadChars(n - 1));
+            //Fila inferior, If CharsOmitidos < Key |=>| llenar última, Else llenar última - 1
+            if (CharsOmitidos > llave) ZigZag[llave - 1] = GetString(br.ReadChars(n - 2));
+            else ZigZag[llave - 1] = GetString(br.ReadChars(n - 1));
 
             return ZigZag;
         }
@@ -148,49 +143,35 @@ namespace Library_LAB4
         {
             try
             {
+                int TotalChars = File.ReadAllText(rutaCifrado).Length;
                 using var Fs = new FileStream(rutaCifrado, FileMode.Open);
                 using BinaryReader Br = new BinaryReader(Fs);
                 using FileStream writeStream = new FileStream($"{rutaDescifrado}/{nombreArchivo}.txt", FileMode.OpenOrCreate);
                 using BinaryWriter Bw = new BinaryWriter(writeStream);
 
-                var byteBuffer = new byte[buffer];
+                string[] ZigZag;
+                ZigZag = DescifrarTexto(Br, llave, TotalChars);
 
-                while (Br.BaseStream.Position != Br.BaseStream.Length)
+                int current = 0;
+                while (ZigZag[current].Length != 0)
                 {
-                    byteBuffer = Br.ReadBytes(buffer);
-                    //foreach (var item in byteBuffer)
-                    //{
-
-                    //}
-
-                    // llenar tabla zigzag
-                    int TotalChars = File.ReadAllText(rutaCifrado).Length;
-                    string[] ZigZag;
-                    ZigZag = DText(Br, llave, TotalChars);
-
-                    int current = 0;
-                    while (ZigZag[current].Length != 0)
+                    current = 0;
+                    while (ZigZag[current].Length != 0 && current < llave - 1)
                     {
-                        current = 0;
-                        while (ZigZag[current].Length != 0 && current < llave - 1)
-                        {
-                            char TEST_CurrentByte = (ZigZag[current][0]);
-                            Bw.Write((byte)(ZigZag[current][0]));
-                            ZigZag[current] = ZigZag[current].Substring(1);
-                            current++;
-                        }
+                        char TEST_CurrentByte = (ZigZag[current][0]);
+                        Bw.Write((byte)(ZigZag[current][0]));
+                        ZigZag[current] = ZigZag[current].Substring(1);
+                        current++;
+                    }
 
-                        while (ZigZag[current].Length != 0 && current > 0)
-                        {
-                            char TEST_CurrentByte = (ZigZag[current][0]);
-                            Bw.Write((byte)(ZigZag[current][0]));
-                            ZigZag[current] = ZigZag[current].Substring(1);
-                            current--;
-                        }
+                    while (ZigZag[current].Length != 0 && current > 0)
+                    {
+                        char TEST_CurrentByte = (ZigZag[current][0]);
+                        Bw.Write((byte)(ZigZag[current][0]));
+                        ZigZag[current] = ZigZag[current].Substring(1);
+                        current--;
                     }
                 }
-
-                
 
                 llave = 0;
             }
